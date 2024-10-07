c6dfffc7af0ac5dc96689e447a1abc15fd8cac05
Yoshiya_mac <wanron03@gmail.com>
2024-10-07 10:54:56 +0900

    added price_train function and value.py file

--- nn_solution/simulation_KT.py
+++ nn_solution/simulation_KT.py
@@ -56,3 +56,69 @@ def simul_k(n_sample, T, mparam, policy, policy_type, price_fn, state_init=None,
     
     simul_data = {"price": price, "wage": wage, "v0": v0, "k_cross": k_cross, "ashock": ashock}
             # 384*T, 384*T, 384*50*T, 384*50*T, 384*T
+    return simul_data
+def price_train(n_sample, T, mparam, policy, policy_type, price_fn, state_init=None, shocks=None):
+    loss = price_loss(n_sample, T, mparam, policy, policy_type, price_fn, state_init, shocks)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    
+
+
+# value, policyが学習されないようにする必要あり。
+def price_loss(n_sample, T, mparam, policy, policy_type, price_fn, state_init=None, shocks=None):
+    if shocks:
+        ashock = shocks
+    
+        assert n_sample == ashock.shape[0], "n_sample is inconsistent with given shocks."
+        assert T == ashock.shape[1], "T is inconsistent with given shocks."
+        if state_init:
+            assert np.array_equal(ashock[..., 0:1], state_init["ashock"]) and \
+                "Shock inputs are inconsistent with state_init"
+    else:
+        ashock = simul_shocks(n_sample, T, mparam, state_init)
+    
+    k_cross = np.zeros([n_sample, n_agt, T])
+    if state_init:
+        assert n_sample == state_init["k_cross"].shape[0], "n_sample is inconsistent with state_init."
+        k_cross[:, :, 0] = state_init["k_cross"]
+    else:
+        k_cross[:, :, 0] = mparam.k_ss
+    
+    if policy_type == "nn":
+        for t in range(1, T):
+            price = price_fn(k_cross[:, :, t-1])# 384*1
+            wage = mparam.eta / price # 384*1
+            yterm = ashock[:, t-1] * k_cross[:, :, t-1]**mparam.theta # 384*50
+            n = (mparam.nu * yterm / wage)**(1 / (1 - mparam.nu))
+            y = yterm * n**mparam.nu
+            k_cross_pre = k_cross[:, :, t-1]
+            k_cross[:, :, t] = policy(k_cross[:, :, t - 1], ashock[:, t - 1])# 384*50
+            a_tmp = torch.repeat_interleave(ashock[:, t:t+1], 50, dim=1)#samplerで作成したbatch_size, self.t_unrollのashockを使っている。
+            a_tmp = torch.unsqueeze(a_tmp, 2)
+            basic_s_tmp = torch.cat([torch.unsqueeze(k_cross[:,:,t], axis=-1), a_tmp], axis=-1)
+            full_state_dict = {
+                "basic_s": basic_s_tmp,
+                "agt_s": self.init_ds.normalize_data(torch.unsqueeze(k_cross[:,:,t], axis=-1), key="agt_s", withtf=True)
+            }
+            e0 = -mparam.GAMY * price * k_cross + mparam.BETA * value(full_state_dict)
+            basic_s_tmp_pre = torch.cat([torch.unsqueeze(k_cross_pre, axis=-1), a_tmp], axis=-1)
+            full_state_dict_pre = {
+                "basic_s": basic_s_tmp_pre,
+                "agt_s": self.init_ds.normalize_data(torch.unsqueeze(k_cross_pre, axis=-1), key="agt_s", withtf=True)
+            }
+            e1 = mparam.p * (1 - mparam.delta) * k_cross_pre + mparam.BETA * value(full_state_dict_pre)
+            xitemp = (e0 - e1)/(price * wage)
+            xi = min(B, max(0, xitemp))
+            alpha = xi / B
+            inow = alpha * (mparam.GAMY * k_cross - (1 - mparam.delta) * k_cross_pre)
+            ynow = ashock[:, t-1] * k_cross_pre**mparam.theta * n**mparam.nu
+            nnow = n + xi**2/(2*B)
+        
+        Inow = torch.sum(inow, axis=1)
+        Ynow = torch.sum(ynow, axis=1)
+        Cnow = Ynow - Inow
+        price1 = 1 / Cnow #n_sample, Tになってて欲しい。
+        loss = torch.mean((price - price1)**2)
+
+    return  loss
---
+++ nn_solution/value.py
@@ -0,0 +1,87 @@
+import numpy as np
+import util
+import torch
+import torch.optim as optim
+import torch.nn.functional as F
+
+DTYPE = "float32"
+if DTYPE == "float64":
+    NP_DTYPE = np.float64
+    TORCH_DTYPE = torch.float64  # PyTorchのデータ型を指定
+elif DTYPE == "float32":
+    NP_DTYPE = np.float32
+    TORCH_DTYPE = torch.float32  # PyTorchのデータ型を指定
+else:
+    raise ValueError("Unknown dtype.")
+
+class ValueTrainer():
+    def __init__(self, config):
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.config = config
+        self.value_config = config["value_config"]
+        d_in = config["n_basic"] + config["n_fm"] + config["n_gm"]
+        self.model = util.FeedforwardModel(d_in, 1, self.value_config, name="v_net").to(self.device)
+        self.gm_model = util.GeneralizedMomModel(1, config["n_gm"], config["gm_config"], name="v_gm").to(self.device)
+            # 両方のモデルのパラメータを集める
+        params = list(self.model.parameters()) + list(self.gm_model.parameters())
+
+        self.train_vars = None
+        self.optimizer = optim.Adam(
+            params, 
+            lr=self.value_config["lr"], 
+            eps=1e-8, 
+            betas=(0.99, 0.99)
+        )
+    
+    def prepare_state(self, input_data):
+        state = torch.cat([input_data["basic_s"][..., 0:1], input_data["basic_s"][..., 2:]], dim=-1)
+        gm = self.gm_model(input_data["agt_s"])
+        state = torch.cat([state, gm], dim=-1)
+        return state
+    
+    def value_fn(self, input_data):
+        state = self.prepare_state(input_data)
+        value = self.model(state)
+        return value
+    
+    def loss(self, input_data):
+        y_pred = self.value_fn(input_data)
+        y = input_data["value"]
+        loss = F.mse_loss(y_pred, y)
+        return loss
+    
+    def train(self, train_dataset, valid_dataset, num_epoch=None, batch_size=None):
+            for epoch in range(num_epoch + 1):
+                for train_data in train_dataset:
+                    train_data = {key: value.to(device=self.device, dtype=TORCH_DTYPE) for key, value in train_data.items()}
+                    self.optimizer.zero_grad()  # 勾配をゼロリセット
+                    loss_dict = self.loss(train_data)  # lossメソッドを使用して損失を計算
+                    loss = loss_dict["loss"]
+                    loss.backward()  # 勾配計算
+                    self.optimizer.step()  # パラメータ更新
+
+                # Validation loop
+                if epoch % 20 == 0:
+                    with torch.no_grad():  # 検証時には勾配計算を行わない
+                        for valid_data in valid_dataset:
+                            valid_data = {key: value.to(self.device, dtype=TORCH_DTYPE) for key, value in valid_data.items()}
+                            val_loss_dict = self.loss(valid_data)  # lossメソッドを使用
+                            val_loss = val_loss_dict["loss"]
+                            print(f"Value function learning epoch: {epoch}, validation loss: {val_loss.item()}")
+
+    def save_model(self, path="value_model.pth"):
+    # モデルの重みを保存
+        torch.save(self.model.state_dict(), path)
+        # n_gmが0より大きい場合、gm_modelも保存
+        if self.config["n_gm"] > 0:
+            torch.save(self.gm_model.state_dict(), path.replace(".pth", "_gm.pth"))
+
+    def load_model(self, path):
+    # モデルの重みをCPU上にロード
+        self.model.load_state_dict(torch.load(path, map_location=torch.device('cpu')))
+        self.model.to(torch.device('cpu'))  # モデル自体もCPUに移動
+        
+        # n_gmが0より大きい場合、gm_modelも読み込み
+        if self.config["n_gm"] > 0:
+            self.gm_model.load_state_dict(torch.load(path.replace(".pth", "_gm.pth"), map_location=torch.device('cpu')))
+            self.gm_model.to(torch.device('cpu'))  # gm_modelもCPUに移動
