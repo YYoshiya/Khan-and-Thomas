6b3ba612a06ca8b8dcd7ba9587b6f41e609a2e4d
YYoshiya <0921tynk@keio.jp>
2024-10-09 09:21:33 +0900

    Refactor param.py: Add KTParam class and initialize parameters

---
+++ nn_solution/param.py
@@ -0,0 +1,21 @@
+
+
+class KTParam():
+    def __init__(self, n_agt, beta, mats_path):
+        self.mats_path = mats_path
+        self.n_agt = n_agt
+        self.beta = beta
+        self.theta = 0.3250
+        self.nu = 0.580
+        self.delta = 0.060
+        self.GAMY = 1.0160
+        self.BETA = 0.9540
+        self.eta = 3.6142
+        self.B = 0.002
+        self.Z = np.array([0.9328, 0.9658, 1.0000, 1.0354, 1.0720])
+        self.nz = 5
+        self.Pi = Pi = np.array([[0.8537, 0.1377, 0.0083, 0.0002, 0.0000],
+                                [0.0344, 0.8579, 0.1035, 0.0042, 0.0001],
+                                [0.0014, 0.0690, 0.8593, 0.0690, 0.0014],
+                                [0.0001, 0.0042, 0.1035, 0.8579, 0.0344],
+                                [0.0000, 0.0002, 0.0083, 0.1377, 0.8537]])
