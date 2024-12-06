f5b7487eefeed5f773573a0f9155f41155774a2f
YYoshiya <0921tynk@keio.jp>
2024-12-06 16:40:37 +0900

    損失計算にペナルティ項を追加し、合計損失を返すように変更。データセットの返却時に100番目からの履歴を返すように調整。

--- value_nn/pred_train.py
+++ value_nn/pred_train.py
@@ -58,14 +58,14 @@ def price_loss(nn, data, params):#k_gridに関してxiを求める他は適当�
     Cagg = torch.clamp(Cagg, min=0.1)
     target = 1 / Cagg
     loss = F.huber_loss(price, target.unsqueeze(-1))
-    #min_Iagg = params.min_Iagg  # Define a minimum threshold in params
-    #penalty_weight = params.penalty_weight  # Define penalty weight in params
-    #penalty = torch.relu(min_Iagg - Iagg)  # batch
-    #penalty = penalty * penalty_weight 
-    #penalty_Iagg = torch.mean(penalty)
+    min_Iagg = params.min_Iagg  # Define a minimum threshold in params
+    penalty_weight = params.penalty_weight  # Define penalty weight in params
+    penalty = torch.relu(min_Iagg - Iagg)  # batch
+    penalty = penalty * penalty_weight 
+    penalty_Iagg = torch.mean(penalty)
     # Combine the main loss with the penalty
-    #total_loss = loss + penalty_Iagg
-    return loss
+    total_loss = loss + penalty_Iagg
+    return total_loss
 
 def price_train(data, params, nn, optimizer, num_epochs, batch_size, T, threshold):
     ashock_data = vi.generate_ashock(1, T, params.ashock, params.pi_a).squeeze(0).unsqueeze(-1).expand(-1, params.ishock.size(0))#G, 5
--- value_nn/value_iter.py
+++ value_nn/value_iter.py
@@ -558,12 +558,12 @@ def get_dataset(params, T, nn, num_sample, p_init=None):
         a = a_new  # Update aggregate shock if necessary
     move_models_to_device(nn, "cuda")
 
-    return {
-        "grid": k_history,
-        "dist": dist_history,
-        "dist_k": dist_k_history,
-        "grid_k": grid_k_history,
-        "ashock": ashock_history,
+     return {
+        "grid": k_history[100:],         # 100番目から最後まで
+        "dist": dist_history[100:],      # 100番目から最後まで
+        "dist_k": dist_k_history[100:],  # 100番目から最後まで
+        "grid_k": grid_k_history[100:],  # 100番目から最後まで
+        "ashock": ashock_history[100:],  # 100番目から最後まで
     }
 
 
