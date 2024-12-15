fbf41578e9dde83aef4d8679e9abbce8a8fd5ee6
YYOSHIYA <0921tynk@keio.jp>
2024-12-15 10:01:51 +0900

    ブランチを切って1gmにする。

--- value_nn/main.py
+++ value_nn/main.py
@@ -39,8 +39,8 @@ else:
 class ValueNN(nn.Module):
     def __init__(self, d_in):
         super(ValueNN, self).__init__()
-        self.fc1 = nn.Linear(d_in, 64)
-        self.fc2 = nn.Linear(64, 32)
+        self.fc1 = nn.Linear(d_in, 32)
+        self.fc2 = nn.Linear(32, 32)
         self.fc3 = nn.Linear(32, 32)
         self.fc4 = nn.Linear(32, 1)
         self.relu = nn.ReLU()
@@ -67,7 +67,7 @@ class GeneralizedMomModel(nn.Module):
         x = self.relu(self.fc1(x))
         x = self.relu(self.fc2(x))
         x = self.relu(self.fc3(x))
-        x = self.relu(self.fc4(x))
+        x = self.fc4(x)
         return x #このあとこれと分布の内積をとる。
     
 class Price_GM(nn.Module):
@@ -80,11 +80,12 @@ class Price_GM(nn.Module):
         self.relu = nn.ReLU()
         self.tanh = nn.Tanh()
         self.softplus = nn.Softplus()
+        self.leakyrelu = nn.LeakyReLU()
     def forward(self, x):
         x = self.relu(self.fc1(x))
         x = self.relu(self.fc2(x))
         x = self.relu(self.fc3(x))
-        x = self.relu(self.fc4(x))
+        x = self.fc4(x)
         return x #このあとこれと分布の内積をとる。
 
 class NextkNN(nn.Module):
@@ -233,7 +234,7 @@ class nn_class:
         self.optimizer_policyinit = optim.Adam(self.policy.parameters(), lr=0.001)
         self.optimizer_val = optim.Adam(self.params_value, lr=0.0004)
         self.optimizer_pol = optim.Adam(self.params_policy, lr=0.0004)
-        self.optimizer_pri = optim.Adam(self.params_price, lr=0.001)
+        self.optimizer_pri = optim.Adam(self.params_price, lr=0.01)
         self.optimizer_next_gm = optim.Adam(self.params_next_gm, lr=0.01)
 
 def initialize_weights(model):
